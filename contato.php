5a69c58a865b04f2daac5d8b5bc7e296b4523754
Fhiranoyama <162184595+Fhiranoyama@users.noreply.github.com>
2024-11-06 12:51:27 -0300

    Add files via upload

--- contato.php
+++ contato.php
@@ -3,13 +3,22 @@ include_once('templates/topo.php');
 ?>
 <div class="conteudo">
     <h1>Contato</h1>
-    <form onsubmit="showPopup(); return false;">
+    <form action="admin/inserir.php" method="POST">
+        <div>
+            <label for="nome">Nome:</label>
+            <input type="text" id="nome" name="nome" placeholder="Seu nome" required>
+        </div>
         <div>
             <label for="email">Email:</label>
             <input type="email" id="email" name="email" placeholder="E-mail para contato" required>
         </div>
         <div>
+            <label for="assunto">Assunto:</label>
+            <input type="text" id="assunto" name="assunto" placeholder="Assunto da mensagem" required>
+        </div>
+        <div>
             <label for="mensagem">Mensagem:</label>
+			<br>
             <textarea id="mensagem" name="mensagem" rows="6" placeholder="Podemos ajudar?" required></textarea>
         </div>
         <div>
@@ -20,10 +29,3 @@ include_once('templates/topo.php');
 <?php
 include_once('templates/rodape.php'); 
 ?>
-
-<script>
-function showPopup() {
-    alert("Nos comunicaremos em breve!");
-    window.location.href = "index.php"; // Redireciona para a página inicial
-}
-</script>
---
+++ db2.sql
@@ -0,0 +1,61 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 06/11/2024 às 16:09
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `db2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `contato`
+--
+
+CREATE TABLE `contato` (
+  `id` int(11) NOT NULL,
+  `nome` text NOT NULL,
+  `email` text NOT NULL,
+  `assunto` text NOT NULL,
+  `mensagem` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `contato`
+--
+ALTER TABLE `contato`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `contato`
+--
+ALTER TABLE `contato`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
